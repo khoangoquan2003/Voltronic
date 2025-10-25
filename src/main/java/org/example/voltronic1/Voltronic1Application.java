79d29bcf900f1cd34e61053a8e25b9d5ab4512ba
Khoang Quan <your-email@example.com>
2025-10-25 22:53:06 +0700

    Initial commit

---
+++ src/main/java/org/example/voltronic1/Voltronic1Application.java
@@ -0,0 +1,13 @@
+package org.example.voltronic1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Voltronic1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Voltronic1Application.class, args);
+    }
+
+}
